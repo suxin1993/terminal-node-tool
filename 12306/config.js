fa165a67feccbffeabe9503a1dd89ba766995e0a
suben <18565641627@163.com>
2023-01-10 20:19:20 +0800

    fix: 添加config配置

---
+++ 12306/config.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: suben 18565641627@163.com
+ * @Date: 2023-01-10 17:28:30
+ * @LastEditors: suben 18565641627@163.com
+ * @LastEditTime: 2023-01-10 20:18:54
+ * @FilePath: \terminal-node-tool\12306\config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+module.exports = {
+    date: '', // 出发日期 格式：yyyy-mm-dd 如：2020-08-31
+    from: '', // 起始站中文
+    to: '', // 到达站中文
+    type: '', // 乘客类型 成人:ADULT，学生:0X00
+    mail: '', // 企业微信地址机器人的webhookurl的key值////必填
+    cookie: '', //去自己的12306的网站去拿一下//必填
+}
