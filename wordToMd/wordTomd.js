70808e3b69c365488e4cba3cf46e2224498529bf
suben <18565641627@163.com>
2023-04-26 21:23:32 +0800

    fix: 修改引入

--- wordToMd/wordTomd.js
+++ wordToMd/wordTomd.js
@@ -2,11 +2,13 @@
  * @Author: suben 18565641627@163.com
  * @Date: 2023-04-26 14:49:27
  * @LastEditors: suben 18565641627@163.com
- * @LastEditTime: 2023-04-26 16:44:32
+ * @LastEditTime: 2023-04-26 21:21:51
  * @FilePath: \terminal-node-tool\wordToMd\wordTomd.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-const mammoth = require("mammoth");
+
+//用了自己魔幻编写的mammoth.js ，不过依旧需要下载mammoth版本的包，有很多依赖mammoth
+const mammoth = require("../mammoth.js/lib/index.js");
 require("colors");
 const { pathJoinDir, exitsFolder, getbaseFiles, findSync, writeFileAsync, readFile,pathBasefilename,parsePath } = require("../utils/node-operate-folder.js")
 let filepath = pathJoinDir(__dirname,'../wordToMd')
@@ -45,7 +47,7 @@ async function wordTomd() {
             let parePath = parsePath(e)
             let oldName = pathBasefilename(e)
             let newFile = pathJoinDir(parePath, `${oldName}.md`)
-            await writeFileAsync(newFile, result.value)
+            await writeFileAsync(newFile, result.value)     
             console.log(`${e}文件已经转化`.bold.blue);
         }
         return '路径下的docx文件已经开始转化'
