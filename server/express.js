05ab17f689d2da085b14c98acf7e273aa7229eb8
suxin <18565641627@.163com>
2023-05-06 20:10:46 +0800

    fix: 添加上传图片请求

--- server/express.js
+++ server/express.js
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2021-09-07 16:58:57
- * @LastEditTime: 2022-12-26 16:05:55
- * @LastEditors: suben 18565641627@163.com
+ * @LastEditTime: 2023-05-06 20:10:12
+ * @LastEditors: suxin 18565641627@.163com
  * @Description: In User Settings Edit
  * @FilePath: /terminal-node-tool/server/express.js
  */
@@ -18,6 +18,8 @@ let server = http.createServer(app)
 let path = require('path')
 
 let root = path.join(__dirname, './views')
+let ImagePath = path.join(__dirname, './uploads')
+let updateImagePath = 'D://uploads'
 
 let fs = require('fs')
 
@@ -47,6 +49,39 @@ app.use(function (req, res, next) {
 //中间件
 app.use(baseLoction, router)
 
+//处理图片
+const multer = require('multer')
+const storage = multer.diskStorage({
+    limits: { fileSize: 10000 * 1024 },
+    destination: function (req, file, cb) {
+        cb(null, updateImagePath)
+    },
+    filename: function (req, file, cb) {
+        let originalname = file.originalname
+        console.error(req.body.name)
+        console.error(originalname)
+        cb(null, req.body.name)
+    },
+})
+
+const upload = multer({ storage: storage })
+router.post(
+    '/upload',
+    upload.fields([
+        { name: 'img', maxCount: 1 },
+        { name: 'name', maxCount: 1 },
+    ]),
+    (req, res, next) => {
+        console.log(req.body)
+        res.send('文件上传成功')
+    }
+)
+router.get(`/updateLocaltion`, function (req, res) {
+    console.error(req.query)
+
+    res.send('请求成功')
+})
+
 router.get(`/time`, function (req, res) {
     let time = new Date().valueOf()
     console.error(new Date().valueOf())
@@ -75,6 +110,9 @@ router.get(`/`, function (req, res) {
 // })
 
 app.use(express.static(root))
+
+//配置图片静态资源
+app.use('/uploads', express.static('uploads'))
 
 server.listen(port, function () {
     console.log(`${getIPAdress()}:${port}`)
